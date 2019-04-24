9fe7b3e8cb18491089613d53a687300a301769b5
AhSatUrnemal <49948060+AhSatUrnemal@users.noreply.github.com>
2019-04-24 15:02:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,8 @@ bot.on('ready', function() {
   console.log("Connecté")
 });
 
+bot.login('NTcwNTk1MjY3OTUxNDYwMzcz.XMBeVg.j9UdtGc30ykdlqHtuoJ01p4ZJkM');
+
 bot.on''message', message -> {
   if (message.content === prefix + "dev")
     message.reply("Je suis actuellement indisponnible.")
